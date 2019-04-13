2525a081ae0329608985586fc3eac509aafee539
unknown <sanjeevdhangar89@gmail.com>
2019-04-14 02:52:23 +0530

    adding interest rate

---
+++ installment_scheduler/fixed principal payment and fixed interest rate.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class data {
+	public:
+		int s_no;
+		float open_balance,principal,interest_rate,interest;
+		float installment,closing_balance;
+}row[1000];
+
+int main()
+{
+	float fixed_principal,fixed_interest,opening_balance;
+	cin>>fixed_principal>>fixed_interest>>opening_balance;
+	
+	int yrs=opening_balance/fixed_principal;
+	
+	
+	
+	
+	
+}
